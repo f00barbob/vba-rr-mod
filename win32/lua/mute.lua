aa723a43b1b2b601933c3903b7e2c17ea61bf766
f00barbob <f00barbob@gmail.com>
2015-04-06 04:39:28 -0500

    Further documentation to mute.lua

--- win32/lua/mute.lua
+++ win32/lua/mute.lua
@@ -5,7 +5,7 @@ local save_ext = "sgm"
 -- the result are save files with a naming format like "Z:\save11370049985.sgm"
 -- the timestamps are so-called epoch times.
 
-local save_interval = 1  -- in minutes
+local save_interval = 20  -- in minutes
 local save_interval = save_interval * 60
 
 local did_save = false
@@ -15,12 +15,14 @@ keyBindings	= {
 	keyUnmute  = "N"
 }
 
-base_time = os.time()  -- get start time in seconds
+base_time = os.time()  -- get start time in seconds since Jan 1 1970
 
 while true do    
     
     inpto	= inpt;
 	inpt	= input.get()
+    
+    -- not neccessary, but these prevent the effects of a key being held continuously for more than 1 frame.
     
 	if  inpt[keyBindings['keyMute']] and not inpto[keyBindings['keyMute']] then
         emu.mute()
@@ -30,14 +32,16 @@ while true do
         emu.unmute()
 	end
     
-    -- subtract base time from current; if divisible by interval, 
+    -- subtract base time from current; modulo per the interval 
     current_interval = (os.time() - base_time) % save_interval
 
+    -- if the remainder of the above is 0, save it and set the flag to prevent 3600 saves
+    --  (1 per frame from for one minute) from being made
     if current_interval == 0
     and did_save == false then
         hax.save(save_path .. "\\" .. save_prefix .. os.time() .. "." .. save_ext)
         did_save = true
-    elseif current_interval ~= 0 then
+    elseif current_interval ~= 0 then -- once the remainder is non-zero, reset the flag
         did_save = false 
     end
         
