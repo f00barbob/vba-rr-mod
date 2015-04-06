f4fef95bc0299f956290322e438760cb95abc0c0
f00barbob <f00barbob@gmail.com>
2015-04-06 04:17:48 -0500

    mute options and autosave script

---
+++ win32/lua/mute.lua
@@ -0,0 +1,45 @@
+local save_path = "z:/" -- don't forget to escape backslashes
+local save_prefix = "mudge"
+local save_ext = "sgm"
+
+local save_interval = 1  -- in minutes
+local save_interval = save_interval * 60
+
+local did_save = false
+
+keyBindings	= {
+	keyMute	= "M",
+	keyUnmute  = "N"
+}
+
+base_time = os.time()  -- get start time in seconds
+
+while true do    
+    
+    inpto	= inpt;
+	inpt	= input.get()
+    
+	if  inpt[keyBindings['keyMute']] and not inpto[keyBindings['keyMute']] then
+        emu.mute()
+	end
+    
+	if  inpt[keyBindings['keyUnmute']] and not inpto[keyBindings['keyUnmute']] then
+        emu.unmute()
+	end
+    
+    -- subtract base time from current; if divisible by interval, 
+    current_interval = (os.time() - base_time) % save_interval
+
+    if current_interval == 0
+    and did_save == false then
+        hax.save(save_path .. "\\" .. save_prefix .. os.time() .. "." .. save_ext)
+        did_save = true
+    elseif current_interval ~= 0 then
+        did_save = false 
+    end
+        
+        
+    
+    emu.frameadvance()
+    
+end
